0721c63263a0c0e69a76faf21edd1d1f93ea5904
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-06 16:03:39 +0530

    home.js

--- Home.js
+++ Home.js
@@ -1,24 +1,16 @@
 import './Home.css';
-import Navbar from "../../component/Navbar/Navbar";
-import Footer from "../../component/Footer/Footer";
-
+import Navbar from "./../../components/Navbar/Navbar";
+import Footer from "./../../components/Footer/Footer";
 
 export default function Home() {
     return( 
-    <>
-
-
-        <div className='home-container'>
-        <Navbar/>
-
-            <h1 className='heading'>Web Design & <br/> <span className='color-heading'>Development</span><br/>Course</h1>
-
-            <p className='text'>Web Design is a specialisation of the design stream . They also use HTML,<br/>
-            CSS, WYSIWYG editing softwere, mark up validators etc.,<br/>
-            to create design elements</p>
-            <button className='btn'> JOIN US</button>
-            <Footer/>
-        </div>
-       </> 
-    )
+        <div className="home-container">
+            <Navbar />
+            <h1>Home</h1>
+            <p>
+                This is the home page. It is a React Component as a view.
+            </p>
+            <Footer />
+       </div> 
+    );
 }
