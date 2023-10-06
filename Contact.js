ad0f2b2fd91bb385d5770b6295ad7af15d832618
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-06 16:20:54 +0530

    contact.js

--- Contact.js
+++ Contact.js
@@ -1,19 +1,16 @@
 import './Contact.css';
-
-import Navbar from "../../component/Navbar/Navbar";
-import Footer from "../../component/Footer/Footer"
+import Navbar from "./../../components/Navbar/Navbar";
+import Footer from "./../../components/Footer/Footer";
 
 export default function Contact() {
     return(
-        <div className='contact-container'>
-            <Navbar/>
-           <div className='contact-card'>
-            <h1 className='contact-heading'>Contact</h1>
-            <input type='text' placeholder='Name' className='text-name'/>  <br/>
-            <input type='gmail' placeholder='Gmail' className='text-name'/> <br/>
-            <button className='contact-btn'>Submit</button>
-           </div>
-            <Footer/>
+        <div className='contact-container'> 
+            <Navbar />
+            <h1>Contact</h1>
+            <p>
+                This is the home page. It is a React Component as a view.
+            </p>
+            <Footer />
         </div>
     )
 }
